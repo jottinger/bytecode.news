8b136b07b9662cc2bba353e25241f0e2841a5420
Joseph Ottinger <joeo@enigmastation.com>
2026-02-08 19:58:22 -0500

    First cut of executable app

---
+++ app/src/main/kotlin/com/enigmastation/streampack/NevetApplication.kt
@@ -0,0 +1,18 @@
+/* Joseph B. Ottinger (C)2026 */
+package com.enigmastation.streampack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.persistence.autoconfigure.EntityScan
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication(scanBasePackages = ["com.enigmastation.streampack"])
+@ConfigurationPropertiesScan("com.enigmastation.streampack")
+@EntityScan("com.enigmastation.streampack")
+@EnableJpaRepositories("com.enigmastation.streampack")
+class NevetApplication
+
+fun main(args: Array<String>) {
+    runApplication<NevetApplication>(*args)
+}
---
+++ app/src/test/kotlin/com/enigmastation/streampack/NevetApplicationTests.kt
@@ -0,0 +1,21 @@
+/* Joseph B. Ottinger (C)2026 */
+package com.enigmastation.streampack
+
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Import
+
+@SpringBootTest
+@Import(TestChannelConfiguration::class)
+class NevetApplicationTests {
+
+    @Autowired lateinit var context: ApplicationContext
+
+    @Test
+    fun `application context loads`() {
+        assertNotNull(context)
+    }
+}
